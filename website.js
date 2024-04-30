7f37c6b3b5d97bbb77f02e37721353356bafece5
unknown <damilolamichael944@gmail.com>
2024-04-30 09:28:16 +0100

    resetCart

--- website.js
+++ website.js
@@ -28,3 +28,19 @@ function addCart() {
     cartQuantity = cartQuantity + 1;
     console.log(`Cart quantity: ${cartQuantity}`);
 }
+
+function plusCart1() {
+    cartQuantity = cartQuantity + 2;
+    console.log(`Cart quantity: ${cartQuantity}`);
+}
+
+function pluscart2() {
+    cartQuantity = cartQuantity + 3;
+    console.log(`Cart quantity: ${cartQuantity}`);
+}
+
+function resetCart() {
+    cartQuantity = 0;
+    console.log('Cart was reset');
+    console.log(`Cart quantity: ${cartQuantity}`);
+}
