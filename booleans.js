41c6d158a0d453eb92bf00aa969df66396411eae
unknown <damilolamichael944@gmail.com>
2024-05-07 18:47:20 +0100

    booleans and if statment

--- booleans.js
+++ booleans.js
@@ -28,3 +28,34 @@ console.log(0.2 >= 0 && 0.2 < 1 / 3);
 
 console.log(true || false);
 console.log(!true);
+
+
+
+if (0) {
+    console.log('truthy');
+}
+
+const cartQuanty = 5;
+
+if (cartQuanty) {
+    console.log('cart is full');
+}
+
+console.log(!0);
+
+console.log('nice' / 6);
+
+let boolean1;
+console.log(boolean1);
+
+
+const result = 0 ? 'truthy' : 'falsy';
+console.log(result);
+
+false && console.log('Yooo');
+
+const message = 3 && 'Yooo';
+console.log(message);
+
+const currency = undefined || 'USD';
+console.log(currency);
